831e114c92adfb254077ffa06d7c44570cbff4e7
Giorgos Dimitriou <giwrgosdi@gmail.com>
2019-10-03 18:34:02 +0100

    Add vendor and go mod/sum Add API package Add main as a playground file

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/giorgosdi/assume-role
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.24.2
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20191002035440-2ec189313ef0 // indirect
+)
---
+++ pkg/api/api.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+)
+
+func createSession(profile string) (*sts.STS, error) {
+	// stsClient := session.Must(session.NewSession())
+	stsSession, err := session.NewSessionWithOptions(session.Options{
+		Profile: profile,
+	})
+	if err != nil {
+		return nil, err
+	}
+	stsSess := sts.New(stsSession)
+	return stsSess, nil
+}
+
+func constractSessionAndArn(state map[string]string) (string, string) {
+	roleArn := fmt.Sprintf("arn:aws:iam::%s:role/%s", state["account"], state["role"])
+	roleSessionName := fmt.Sprintf("role-%s-%s", state["profile"], state["role"])
+	return roleArn, roleSessionName
+}
+
+func AssumeRole(state map[string]string) (*sts.AssumeRoleOutput, error) {
+
+	stsSess, err := createSession(state["profile"])
+	if err != nil {
+		return nil, err
+	}
+	roleArn, roleSessionName := constractSessionAndArn(state)
+
+	role := &sts.AssumeRoleInput{
+		RoleArn:         aws.String(roleArn),
+		RoleSessionName: aws.String(roleSessionName),
+	}
+	resp, err := stsSess.AssumeRole(role)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
